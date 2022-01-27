7d9effd0c9262f2dadb18179fec1e21228efd59f
rakitenkogermans <rakitenko.germans@gmail.com>
2022-01-27 20:18:12 +0200

    Map component

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import {useMediaQuery} from "@mui/material";
+import useStyles from "../Map/styles";
+import GoogleMapReact from 'google-map-react';
 
 const Map = () => {
+    const classes = useStyles();
+    const isMobile = useMediaQuery('(min-width:600px)');
+
+    const coordinates = { lat: 0, lng: 0};
+
     return (
-        <div>
-            m
+        <div className={classes.mapContainer}>
+            <GoogleMapReact
+                bootstrapURLKeys={{ key: 'AIzaSyCgC03RFj7Pv_ay9oaM-izTRBHsq8gA4uQ' }}
+                defaultCenter={coordinates}
+                center={coordinates}
+                defaultZoom={14}
+                margin={[50,50,50,50]}
+
+            >
+
+            </GoogleMapReact>
         </div>
     );
 };
