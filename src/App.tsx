b2b1f1f60124372d94f3543b079e86ff0fda25bf
rakitenkogermans <rakitenko.germans@gmail.com>
2022-01-27 19:03:15 +0200

    Header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import {CssBaseline, Grid} from "@mui/material";
+import {createTheme, CssBaseline, Grid} from "@mui/material";
 import Header from "./components/Header/Header";
 import Map from "./components/Map/Map";
 import List from "./components/List/List";
+import {ThemeProvider} from "@mui/styles";
 
 const App = () => {
+    const theme = createTheme();
     return (
         <>
-            <CssBaseline />
-            <Header />
-            <Grid container spacing={3} style={{width: '100%'}}>
-                <Grid item xs={12} md={4}>
-                    <List />
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <Header />
+                <Grid container spacing={3} style={{width: '100%'}}>
+                    <Grid item xs={12} md={4}>
+                        <List />
+                    </Grid>
+                    <Grid item xs={12} md={8}>
+                        <Map />
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} md={8}>
-                    <Map />
-                </Grid>
-            </Grid>
+            </ThemeProvider>
         </>
     );
 };
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,77 @@
 import React from 'react';
+import {AppBar, Box, InputBase, Toolbar, Typography} from "@mui/material";
+import { styled, alpha } from '@mui/material/styles';
+import {Autocomplete} from "@react-google-maps/api";
+import SearchIcon from '@mui/icons-material/Search';
+import useStyles from "./styles"
+
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+        backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+        marginLeft: theme.spacing(3),
+        width: 'auto',
+    },
+}));
+
+const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+        padding: theme.spacing(1, 1, 1, 0),
+        // vertical padding + font size from searchIcon
+        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: '20ch',
+        },
+    },
+}));
 
 const Header = () => {
+    const classes = useStyles();
+    console.log(classes);
     return (
-        <div>
-            h
-        </div>
+        <AppBar position="fixed">
+            <Toolbar className={classes.toolbar}>
+                <Typography variant="h5" className={classes.title}>
+                    Travel Map
+                </Typography>
+                <Box display="flex">
+                    <Typography variant="h6" className={classes.title}>
+                        Find new places
+                    </Typography>
+                    {/*<Autocomplete>*/}
+                        <Search>
+                            <SearchIconWrapper>
+                                <SearchIcon />
+                            </SearchIconWrapper>
+                            <StyledInputBase
+                                placeholder="Search…"
+                                inputProps={{ 'aria-label': 'search' }}
+                            />
+                        </Search>
+                    {/*</Autocomplete>*/}
+                </Box>
+            </Toolbar>
+        </AppBar>
     );
 };
 
