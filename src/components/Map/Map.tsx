dafe021d9f70aaae98014046004e9e969a996f5f
rakitenkogermans <rakitenko.germans@gmail.com>
2022-01-29 20:38:27 +0200

    change handler, props

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, {FC} from 'react';
 import {useMediaQuery} from "@mui/material";
 import useStyles from "../Map/styles";
 import GoogleMapReact from 'google-map-react';
+import {IBounds, ICoordinates} from "../../interfaces/Places";
 
-const Map = () => {
+export interface MapProps {
+    setCoordinates: (value: ICoordinates | ((prevVar: ICoordinates ) => ICoordinates)) => void;
+    setBounds: (value: IBounds | null | ((prevVar: IBounds | null) => IBounds | null)) => void;
+    coordinates: ICoordinates;
+}
+
+const Map: FC<MapProps> = ({ setCoordinates, setBounds, coordinates }) => {
     const classes = useStyles();
     const isMobile = useMediaQuery('(min-width:600px)');
 
-    const coordinates = { lat: 0, lng: 0};
+    // const coordinates = { lat: 0, lng: 0};
+    const mapChangeHandler = (event: any) => {
+        console.log(event);
+        setCoordinates({ lat: event.center.lat, lng: event.center.lng});
+        setBounds({ ne: event.marginBounds.ne, sw: event.marginBounds.sw});
+    }
 
     return (
         <div className={classes.mapContainer}>
@@ -17,7 +29,7 @@ const Map = () => {
                 center={coordinates}
                 defaultZoom={14}
                 margin={[50,50,50,50]}
-
+                onChange={mapChangeHandler}
             >
 
             </GoogleMapReact>
